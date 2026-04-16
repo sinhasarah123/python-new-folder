ccd967a46fd4f38939d6391a4369a2e94af84f80
sarahsinha123 <sinhasarah47@gmail.com>
2026-04-16 21:14:07 +1000

    initiak

--- lesson 12 activity 2/main.py
+++ lesson 12 activity 2/main.py
@@ -1,16 +1,14 @@
 def paren(s,l,r,p,n):
-    if(p==2*n):
-        for ss in s:
-            print(ss,end="")
-            print("\n")
-            return
-    if(l<n):
-        s[p]="("
-        paren(s,l+1,r,p+1,n)
+    if(p==2*n):   
+        print("".join(s))
+        return
     if(l>r):
-            s[p]="}"
-            paren(s,l,r+1,p+1,n)
+        s[p]="}"
+        paren(s,l,r+1,p+1,n)
+    if(l<n):
+        s[p]="{"
+        paren(s,l+1,r,p+1,n)
 n=int(input("Enter the number of pairs of parentheses: "))
-s=[""]*(2*n)
+s=[""]*2*n
 print("\n")
 paren(s,0,0,0,n)
