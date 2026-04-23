b4f99c2fe1510f407f1ea3a2c739ea6950958874
sarahsinha123 <sinhasarah47@gmail.com>
2026-04-23 21:04:41 +1000

    ini

---
+++ lesson 142 activity 1/main.py
@@ -0,0 +1,14 @@
+def minelement(a, size):
+    temp =a[0]
+    for i in range (1,size):
+        temp=min(temp,a[i])
+    return temp
+def maxlement(a, size):
+    temp =a[0]
+    for i in range (1,size):
+        temp=min(temp,a[i])
+    return temp
+a=[1,2,3,4,5]
+size = len(a)
+print("Minimum element is ", minelement(a,size))
+print("Maximum element is ", maxlement(a,size))
