9c1c49132c2d48039b3b91c4d8b0bf8e294f2ddc
sarahsinha123 <sinhasarah47@gmail.com>
2026-06-14 15:32:19 +0530

    prime

---
+++ hw26/main.py
@@ -0,0 +1,10 @@
+for num in range(10, 100):
+    prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            prime = False
+            break
+
+    if prime:
+        print(num)
