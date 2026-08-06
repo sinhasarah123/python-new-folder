25a8a31ffaa3ca84de13847f7912d34b3db3c35f
sarahsinha123 <sinhasarah47@gmail.com>
2026-08-06 16:24:07 +0530

    elo

---
+++ activitysql/main.py
@@ -0,0 +1,7 @@
+print("What is the type of database given above -\n 1) Relational Database\n 2) Non-Relational Database")
+answer=int(input("Enter your guess here..."))
+if answer==2:
+    print("you guessed right!")
+else:
+    print("maybe next time you guessed wrong!")
+print("\nPlease tell your teacher why you guessed this?")
